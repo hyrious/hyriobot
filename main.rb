c912588a4779743f7f2221f911fcc288da7c6ef4
hyrious <hyrious@outlook.com>
2018-02-22 13:16:57 +0800

    use cxxabi to show the type

--- main.rb
+++ main.rb
@@ -23,15 +23,21 @@ end
 
 # -win32const name
 require 'tempfile'
-R /^-win32const\s+(\w+)$/ do |match:, qq:, **|
-  next unless P.user_has_privilege(qq, 'eval')
-  Tempfile.open(['a-', '.c'], 'tmp') do |f|
-    f.write "#define S(s) X(s)\n#define X(s) #s\n#include<windows.h>\n#include<d3d11.h>\nmain(){puts(("" S(#{match[1]})));}"
-    f.close
-    Tempfile.open(['a-', '.exe'], 'tmp') do |o|
-      o.close
-      raw = `2>&1 gcc -w -O -m32 #{f.path.inspect} -o #{o.path.inspect} && #{o.path.inspect}`.encode("UTF-8", "GBK", replace: '?', invalid: :replace, undef: :replace, fallback: '?').chomp("\n")
-      raw
+R /^-win32const\s+(\w+)$/ do |match:, **|
+  D {
+    Tempfile.open(['a-', '.cpp'], 'tmp') do |f|
+      f.write "#define S(s) X(s)\n#define X(s) #s\n#include<windows.h>\n#include<d3d11.h>\n#include<bits/stdc++.h>\nint main(){puts(("" S(#{match[1]})));}"
+      f.close
+      Tempfile.open(['a-', '.exe'], 'tmp') do |o|
+        o.close
+        raw = `2>&1 g++ -w -O -m32 #{f.path.inspect} -o #{o.path.inspect} && #{o.path.inspect}`.encode("UTF-8", "GBK", replace: '?', invalid: :replace, undef: :replace, fallback: '?').chomp("\n")
+        if raw == match[1]
+          open(f,'w'){|g|g.write "#include<windows.h>\n#include<d3d11.h>\n#include<cxxabi.h>\n#include<bits/stdc++.h>\nint main(){int status;char*name=abi::__cxa_demangle(typeid(#{match[1]}).name(),0,0,&status);puts(name);free(name);}"}
+          `2>&1 g++ -w -O -m32 #{f.path.inspect} -o #{o.path.inspect} && #{o.path.inspect}`.encode("UTF-8", "GBK", replace: '?', invalid: :replace, undef: :replace, fallback: '?').chomp("\n")
+        else
+          raw
+        end
+      end
     end
-  end
+  }
 end
