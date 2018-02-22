e152b3f44e0adea7966e7e7310dd42608bf9150d
hyrious <hyrious@outlook.com>
2018-02-22 13:42:54 +0800

    add cache

--- main.rb
+++ main.rb
@@ -24,6 +24,9 @@ end
 # -win32const name
 require 'tempfile'
 R /^-win32const\s+(\w+)$/ do |match:, **|
+  next unless P.user_has_privilege(qq, 'eval')
+  x = LocalStorage.get("glob", ["cache", "win32const", match[1]])
+  next x if x
   D {
     Tempfile.open(['a-', '.cpp'], 'tmp') do |f|
       f.write "#define S(s) X(s)\n#define X(s) #s\n#include<windows.h>\n#include<d3d11.h>\n#include<bits/stdc++.h>\nint main(){puts(("" S(#{match[1]})));}"
@@ -31,12 +34,14 @@ R /^-win32const\s+(\w+)$/ do |match:, **|
       Tempfile.open(['a-', '.exe'], 'tmp') do |o|
         o.close
         raw = `2>&1 g++ -w -O -m32 #{f.path.inspect} -o #{o.path.inspect} && #{o.path.inspect}`.encode("UTF-8", "GBK", replace: '?', invalid: :replace, undef: :replace, fallback: '?').chomp("\n")
-        if raw == match[1]
+        ans = if raw == match[1]
           open(f,'w'){|g|g.write "#include<windows.h>\n#include<d3d11.h>\n#include<cxxabi.h>\n#include<bits/stdc++.h>\nint main(){int status;char*name=abi::__cxa_demangle(typeid(#{match[1]}).name(),0,0,&status);puts(name);free(name);}"}
           `2>&1 g++ -w -O -m32 #{f.path.inspect} -o #{o.path.inspect} && #{o.path.inspect}`.encode("UTF-8", "GBK", replace: '?', invalid: :replace, undef: :replace, fallback: '?').chomp("\n")
         else
           raw
         end
+        LocalStorage.set("glob", ["cache", "win32const", match[1]], ans)
+        ans
       end
     end
   }
