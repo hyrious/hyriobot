ee281508d4d07f9b763aebed925c29c60f189e1d
hyrious <hyrious@outlook.com>
2018-02-26 17:10:50 +0800

    -O2

--- main.rb
+++ main.rb
@@ -7,7 +7,7 @@ R /^-disasm\s+(.+)$/m do |match:, **|
     f.close
     Tempfile.open(['a-', '.o'], 'tmp') do |o|
       o.close
-      raw = `2>&1 gcc -c -g -w -O -m32 #{f.path.inspect} -o #{o.path.inspect} && objdump -S -M intel #{o.path.inspect}`.encode("UTF-8", "GBK", replace: '?', invalid: :replace, undef: :replace, fallback: '?').chomp("\n")
+      raw = `2>&1 gcc -c -g -w -O2 -m32 #{f.path.inspect} -o #{o.path.inspect} && objdump -S -M intel #{o.path.inspect}`.encode("UTF-8", "GBK", replace: '?', invalid: :replace, undef: :replace, fallback: '?').chomp("\n")
       m = raw.match /^Disassembly of section \.text:\s+(.+?)\n\S+?\s\<_main\>/m
       m ? m[1].gsub("\n\n", "\n").chomp : raw
     end
